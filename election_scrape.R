c413ae0b06689175c30d33fb0dfd6e3f350a1a87
actions-user <actions@github.com>
2023-11-08 03:58:45 +0000

    change script election scrape

--- election_scrape.R
+++ election_scrape.R
@@ -4,47 +4,32 @@ library(dplyr)
 library(data.table)
 
 
-
-# Read the HTML content of the website https://electionresults.sos.mn.gov/Results/Index?ersElectionId=156&scenario=ResultsByPrecinctCrosstab&OfficeInElectionId=33119&QuestionId=0
+# Read the HTML content of the website
 webpage <- read_html("https://electionresults.sos.mn.gov/Results/Index?ersElectionId=157&scenario=ResultsByPrecinctCrosstab&OfficeInElectionId=33119&QuestionId=0")
-table_node <- html_nodes(webpage, "table") 
 
-# Extract the table content 
-table_content <- html_table(table_node)[[3]] 
+# Extract the table content
+table_content <- webpage %>%
+  html_table(fill = TRUE) %>%
+  .[[3]]  # Assuming the table you need is the third one
 
-# Remove last row
-table_content <- table_content %>% filter(row_number() <= n()-1)
+# Remove the last row
+table_content <- table_content[-nrow(table_content), ]
 
-table_content[] <- lapply(table_content, function(cell) {
-  sub("St. Louis: ", "", cell)
-})
-
-table_content[] <- lapply(table_content, function(cell) {
-  sub("ISD #709 - ", "", cell)
-})
-
+# Clean column names
 colnames(table_content) <- gsub("NP", "", colnames(table_content))
+colnames(table_content) <- gsub("ISD #709 - ", "", colnames(table_content))
 
-# Assuming your data frame is named "table_content"
-
-# Identify the columns you want to convert to integers
+# Convert columns to integers
 cols_to_convert <- 2:ncol(table_content)  # Exclude the first column
-
-# Convert the selected columns to integers
 table_content[cols_to_convert] <- lapply(table_content[cols_to_convert], as.integer)
 
-table_content$VoteTotal <- rowSums(table_content[, -1])  # Exclude the first column
-
 # Calculate the percentage for each cell based on the "VoteTotal" column
-for (col in 2:(ncol(table_content) - 1)) {
-  new_col_name <- paste0(names(table_content)[col], "_Percentage")
-  table_content[, new_col_name] <- (table_content[, col] / table_content$VoteTotal) * 100
-}
-
 table_content <- table_content %>%
-  mutate(across(everything(), ~ifelse(. == "NaN", 0, .), .names = "{.col}_New"))
+  mutate(across(starts_with("Col"), ~ifelse(is.na(.), 0, .))) %>%
+  rowwise() %>%
+  mutate(VoteTotal = sum(c_across(starts_with("Col")))) %>%
+  ungroup() %>%
+  mutate(across(starts_with("Col"), ~(. / VoteTotal) * 100, .names = "Col_{.col}_Percentage"))
 
-
-# Write the table to the CSV file
+# Write the table to a CSV file
 write.csv(table_content, "data/table_real.csv", row.names = FALSE)
-
